fa07b3d48550bfd7c43250621fa978ff74616ac6
hyrodium <hyrodium@gmail.com>
2021-04-07 21:50:03 +0900

    fix distance function

--- EquallySpacedPointsOnSO3.jl
+++ EquallySpacedPointsOnSO3.jl
@@ -1,8 +1,10 @@
 using Rotations
+using LinearAlgebra
 
-function distance(R1, R2)
+function distance2(R1, R2)
     R = MRP(R1/R2)
-    return sqrt(R.x^2+R.y^2+R.z^2)
+    d = R.x^2+R.y^2+R.z^2
+    return min(d,1/d)
 end
 
 ## examples
@@ -10,7 +12,7 @@ r1 = rand(RotMatrix{3})
 r2 = rand(RotMatrix{3})
 r3 = rand(RotMatrix{3})
 
-distance(r1,r1)
-distance(r1,r2)
-distance(r2,r1)
-distance(r3*r1,r3*r2)
+distance2(r1,r1)
+distance2(r1,r2)
+distance2(r2,r1)
+distance2(r3*r1,r3*r2)
